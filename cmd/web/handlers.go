f1d2662db5a7d4cb498aca48517c2abfc8d6e5db
Pamela Vermeer <vermeerp@gmail.com>
2018-08-20 22:00:33 -0500

    Updated for Chapter 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"strconv"
 )
@@ -9,14 +8,22 @@ import (
 // Home function  writes a plain-text "Hello from Snippetbox"
 // message as the HTTP response body.
 func (app *App) Home(w http.ResponseWriter, r *http.Request) {
-
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.NotFound(w)
 		return
 	}
 
-	app.RenderHTML(w, "home.page.html") // Use the app.RenderHTML() helper.
+	// Fetch a slice of the latest snippets from the database.
+	snippets, err := app.Database.LatestSnippets()
+	if err != nil {
+		app.ServerError(w, err)
+		return
+	}
 
+	// Pass the slice of snippets to the "home.page.html" templates.
+	app.RenderHTML(w, r, "home.page.html", &HTMLData{
+		Snippets: snippets,
+	})
 }
 
 // ShowSnippet handler function.
@@ -37,7 +44,11 @@ func (app *App) ShowSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprint(w, snippet)
+	// Render the show.page.html template, passing in the snippet data wrapped in
+	// our HTMLData struct.
+	app.RenderHTML(w, r, "show.page.html", &HTMLData{
+		Snippet: snippet,
+	})
 }
 
 // NewSnippet handler function.
